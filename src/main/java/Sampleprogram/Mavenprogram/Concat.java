1e090c9c25fe2470cb2b332cca6202192923a5f9
arunmozhim <arunmozhikavin@gmail.com>
2023-07-21 10:05:33 +0530

    programconcat

---
+++ src/main/java/Sampleprogram/Mavenprogram/Concat.java
@@ -0,0 +1,16 @@
+package Sampleprogram.Mavenprogram;
+
+public class Concat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String first="BEASANT";
+System.out.println("first string:"+ first);
+String second="TECHNOLOGY";
+System.out.println("second string:+ second");
+String joinedstring=first.concat(second);
+System.out.println("joined String:"+joinedstring);
+
+	}
+
+}
